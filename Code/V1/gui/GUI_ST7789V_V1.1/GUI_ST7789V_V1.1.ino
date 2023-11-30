1e171446663d4f46ecd80f1b6a7dec49b18ea8a7
BILLILLID <Phill.eligene@gmail.com>
2023-11-29 23:50:53 -0500

    b
    b

--- Code/V1/gui/GUI_ST7789V_V1.1/GUI_ST7789V_V1.1.ino
+++ Code/V1/gui/GUI_ST7789V_V1.1/GUI_ST7789V_V1.1.ino
@@ -24,14 +24,15 @@ OneButton down1(downButtonPin, true);
 OneButton select1(selectButtonPin, true);
 
 String currentFolder = "/";
-String audio;
-String image;
-
 String folders[10] = {};
 String album[20] = {};
+
+String audio = "/";
+String image = "/";
+
 int currentIndex = 0;
 
-bool isAlbums = false;
+bool isAlbums = true;
 bool isTracks = false;
 bool isSettings = false;
 bool isPlaying = false;
@@ -54,7 +55,7 @@ bool tft_output(int16_t x, int16_t y, uint16_t w, uint16_t h, uint16_t* bitmap)
   return 1;
 }
 
-void updateScreen() 
+void openCard() 
 {
   tft.fillScreen(TFT_DARKGREY);
 
@@ -90,109 +91,19 @@ void updateScreen()
   }
   */
 
+  bool isAlbums = true;
+  bool isTracks = false;
+  bool isSettings = false;
+  bool isPlaying = false;
+  
   tft.setTextColor(TFT_WHITE, TFT_DARKGREY);
-  delay(500);
-}
-
-void up()
-{
-  if(currentIndex > 0)
-  {
-    currentIndex -= 1;
-    updateScreen();
-    Serial.print("Current index is: "); Serial.print(currentIndex); Serial.println(". Go up");
-  }
-}
-
-
-void down()
-{
-  if(currentIndex < 10)
-  {
-    currentIndex += 1;
-    updateScreen();
-    Serial.print("Current index is: "); Serial.print(currentIndex); Serial.println(". Go down");
-  }
-  
-}
-
-void select()
-{
-  Serial.print(folders[currentIndex].c_str()); Serial.println(" has been selected");
-  openAlbum();
-}
-
-void selectBack()
-{
-  Serial.println("Back to Albums");
-  updateScreen();
-}
-
-void interface()
-{
-  
-  if (digitalRead(upButtonPin) == LOW) 
-  {
-    if(currentIndex > 0)
-    {
-      currentIndex -= 1;
-      updateScreen();
-      Serial.print("Current index is: "); Serial.println(currentIndex);
-    }
-    delay(20); // Debounce
-  }
-
-  if (digitalRead(downButtonPin) == LOW) 
-  {
-    if(currentIndex < 10)
-    {
-      currentIndex += 1;
-      updateScreen();
-      Serial.print("Current index is: "); Serial.println(currentIndex);
-    }
-    delay(20); // Debounce
-  }
-
-  if (digitalRead(selectButtonPin) == LOW) 
-  {
-    //selectItem();
-    delay(200); // Debounce
-    //updateScreen()
-    Serial.print(folders[currentIndex].c_str()); Serial.println(" has been selected");
-  }
-  
-}
-
-void fileType(String audio)
-{
-  String audioName = audio;
-  String jpg = "jpg";
-  String mp3 = "mp3";
-  String wav = "wav";
-
-  // Check if mainString contains subString
-  if (audioName.indexOf(jpg) != -1) 
-  {
-    Serial.println("File is Jpeg");
-  } 
-  else if (audioName.indexOf(mp3) != -1)
-  {
-    Serial.println("File is mp3");
-  }
-  else if (audioName.indexOf(wav) != -1)
-  {
-    Serial.println("File is wav");
-  }
-  else
-  {
-    Serial.println("File is not of mentioned type");
-  }
+  delay(50);
 }
 
 void openAlbum()
 {
   tft.fillScreen(TFT_DARKGREY);
-  
+
   String albumDir = "/";
   albumDir.concat(folders[currentIndex].c_str());
 
@@ -222,22 +133,218 @@ void openAlbum()
     entry = root.openNextFile();
     currentDisplayIndex++;
   }
+
+  isAlbums = false;
+  isTracks = true;
+  isSettings = false;
+  isPlaying = false;
+
+  tft.setTextColor(TFT_WHITE, TFT_DARKGREY);
+  delay(50);
+}
+
+void up()
+{
+  if (isAlbums)
+  {
+    if(currentIndex > 0)
+    {
+      currentIndex -= 1;
+      openCard();
+      Serial.print("Current index is: "); Serial.print(currentIndex); Serial.println(". Go up");
+    }
+
+  }
+  else if (isTracks)
+  {
+    if(currentIndex > 0)
+    {
+      currentIndex -= 1;
+      openAlbum();
+      Serial.print("Current index is: "); Serial.print(currentIndex); Serial.println(". Go up");
+    }
+
+  }
+    else if (isSettings)
+    {
+
+    }
+    else if (isPlaying)
+    {
+
+    }
+  
+}
+
+
+void down()
+{ 
+    if (isAlbums)
+  {
+    if(currentIndex < 10)
+    {
+      currentIndex += 1;
+      openCard();
+      Serial.print("Current index is: "); Serial.print(currentIndex); Serial.println(". Go down");
+    }
+
+  }
+  else if (isTracks)
+  {
+    if(currentIndex < 10)
+    {
+      currentIndex += 1;
+      openAlbum();
+      Serial.print("Current index is: "); Serial.print(currentIndex); Serial.println(". Go down");
+    }
+
+  }
+    else if (isSettings)
+    {
+
+    }
+    else if (isPlaying)
+    {
+
+    }
+  
+}
+
+void select()
+{
+  if (isSettings)
+  {
+
+  }
+  else if (isAlbums)
+  {
+    Serial.print(folders[currentIndex].c_str()); Serial.println(" has been selected");
+    openAlbum();
+
+  }
+  else if (isTracks)
+  {
+    Serial.print(album[currentIndex].c_str()); Serial.println(" has been selected");
+    //openAlbum();
+    display();
+
+  }
+  
+}
+
+void selectBack()
+{
+
+  if (isAlbums)
+  {
+    Serial.println("Back to Albums");
+    //penCard();
+    
+    isAlbums = true;
+    isTracks = false;
+    isPlaying = false;
+    isSettings = false;
+  }
+  else if (isTracks)
+  {
+    Serial.println("Back to Albums");
+    openCard();
+    
+    isAlbums = true;
+    isTracks = false;
+    isPlaying = false;
+    isSettings = false;
+  }
+  else if (isPlaying)
+  {
+    Serial.println("Back to Tracks");
+    openAlbum();
+
+    isAlbums = false;
+    isTracks = true;
+    isPlaying = false;
+    isSettings = false;
+  }
+}
+
+void interface()
+{
+  
+  if (digitalRead(upButtonPin) == LOW) 
+  {
+    if(currentIndex > 0)
+    {
+      currentIndex -= 1;
+      openCard();
+      Serial.print("Current index is: "); Serial.println(currentIndex);
+    }
+    delay(20); // Debounce
+  }
+
+  if (digitalRead(downButtonPin) == LOW) 
+  {
+    if(currentIndex < 10)
+    {
+      currentIndex += 1;
+      openCard();
+      Serial.print("Current index is: "); Serial.println(currentIndex);
+    }
+    delay(20); // Debounce
+  }
+
+  if (digitalRead(selectButtonPin) == LOW) 
+  {
+    //selectItem();
+    delay(200); // Debounce
+    //openCard()
+    Serial.print(folders[currentIndex].c_str()); Serial.println(" has been selected");
+  }
+  
+}
+
+void fileType(String file)
+{
+  String Name = file;
+  String jpg = "jpg";
+  String mp3 = "mp3";
+  String wav = "wav";
+
+  // Check if mainString contains subString
+  if (Name.indexOf(jpg) != -1) 
+  {
+    Serial.println("File is Jpeg");
+    image.concat(Name);
+  } 
+  else if (audioName.indexOf(mp3) != -1)
+  {
+    Serial.println("File is mp3");
+    audio.concat(Name);
+  }
+  else if (audioName.indexOf(wav) != -1)
+  {
+    Serial.println("File is wav");
+    audio.concat(Name);
+  }
+  else
+  {
+    Serial.println("File is not of mentioned type");
+  }
 }
 
 void display()
 {
   uint32_t t = millis();
-
+  
   // Get the width and height in pixels of the jpeg if you wish
   uint16_t w = 0, h = 0;
-  TJpgDec.getSdJpgSize(&w, &h, "/tpab2.jpg");
+  TJpgDec.getSdJpgSize(&w, &h,  image);
   Serial.print("Width = "); Serial.print(w); Serial.print(", height = "); Serial.println(h);
 
   // Draw the image, top left at 0,0
   
-  TJpgDec.drawSdJpg(30,20, "/tpab2.jpg");
+  TJpgDec.drawSdJpg(30,20, image);
   //tft.loadFont(AA_FONT_LARGE);
-  tft.drawString("To Pimp A Butter Fly", 25, 217, 10);
+  tft.drawString(audio, 25, 217, 10);
 
   // How much time did rendering take
   t = millis() - t;
